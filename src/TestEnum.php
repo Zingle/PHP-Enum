75136cb1fa81064d463c9b9ed07ffe041a695ba0
Zachary Quintana <zquintana@zingleme.com>
2019-03-14 13:58:03 -0700

    Update meta generation

--- src/TestEnum.php
+++ src/TestEnum.php
@@ -10,4 +10,9 @@ class TestEnum extends AbstractEnum
     const TEST_1 = 'test1';
     const TEST_2 = 'test2';
     const TEST_3 = 'test3';
+
+  	// This shouldn't get included in meta
+    private const NOT_INCLUDED = [
+    	'some' => 'value',
+    ];
 }
